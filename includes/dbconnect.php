bd163386fa89c0239701a8faea5a6aba2defd633
overskilled <ouateedemloic@gmail.com>
2023-05-09 10:30:49 +0100

    Set connection to local db

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -3,4 +3,11 @@
 $dbhostname = "localhost";
 $dbservername = "root";
 $dbpassword = "";
-$dbname = ""
+$dbname = "facture";
+
+$conn = mysqli_connect($dbhostname, $dbservername, $dbpassword, $dbname);
+
+if (!$conn) {
+    echo "Connection failed".mysqli_connect_error();
+    
+}
